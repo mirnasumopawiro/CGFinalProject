a91a1cb1f684d48e0286ecb3b4d95b81d2bd5af4
Mirna Sumopawiro <mirna.brighton@gmail.com>
2018-06-05 18:50:50 +0700

    skybox and canvas

--- main.js
+++ main.js
@@ -1,20 +1,42 @@
 document.addEventListener("DOMContentLoaded", function(){
-	var canvas = document.getElementById('renderCanvas');
+	console.log("hai");
+	var canvas = document.getElementById("renderCanvas");
 	var engine = new BABYLON.Engine(canvas, true);
-	var scene = new BABYLON.Scene(engine);
 
-	var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0). scene);
-	light.diffuse = new BABYLON.Color3(0, 0, 0);
-	light.specular = new BABYLON.Color3(0, 0, 0);
-	light.groundColor = new BABYLON.Color3(0, 0, 0);
+	var initScene = function (){
+		var scene = new BABYLON.Scene(engine);
 
-	var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 0, 0.6, 36, new BABYLON.Vector3(0, 0, 0), scene);
-	camera.attachControl(canvas, true);
-	camera.setTarget(BABYLON.Vector3.Zero());
+		var camera = new BABYLON.ArcRotateCamera("camera", 0, 0.6, 36, new BABYLON.Vector3(0, 0, 0), scene);
+		camera.attachControl(canvas, true);
+		camera.setTarget(BABYLON.Vector3.Zero());
 
-	var ground = new BABYLON.Mesh.CreateGround("ground", 550, 550, 300, scene);
-	var groundMat = new BABYLON.StandardMaterial("ground", scene);
-	groundMat.diffuseTexture = new BABYLON.Texture("texture/desertTex.jpg", scene);
-	groundMat.specularColor = new BABYLON.Color3(0, 0, 0);
+		var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0). scene);
+		light.diffuse = new BABYLON.Color3(0, 0, 0);
+		light.specular = new BABYLON.Color3(0, 0, 0);
+		light.groundColor = new BABYLON.Color3(0, 0, 0);
+
+		var ground = new BABYLON.Mesh.CreateGround("ground", 30, 30, 30, scene);
+		var groundMat = new BABYLON.StandardMaterial("ground", scene);
+		groundMat.diffuseTexture = new BABYLON.Texture("texture/desertTex.jpg", this.scene);
+		groundMat.specularColor = new BABYLON.Color3(0, 0, 0);
+		ground.material = groundMat;
+
+		var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:1000.0}, scene);
+        var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+        skyboxMaterial.backFaceCulling = false;
+        skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("skybox/skybox", scene);
+        skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+        skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+        skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+		skybox.material = skyboxMaterial;
+
+		return scene;
+	};
+
+	var scene = initScene();
+
+	engine.runRenderLoop(function (){
+		scene.render();
+	});
 });
 
